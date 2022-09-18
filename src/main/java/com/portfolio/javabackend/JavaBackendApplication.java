91cd8497b9acd42cfe364735489064f6c8d2405b
Sehran <seh96.dev@gmail.com>
2022-09-19 00:44:58 +0800

    added cors origin for login

--- src/main/java/com/portfolio/javabackend/JavaBackendApplication.java
+++ src/main/java/com/portfolio/javabackend/JavaBackendApplication.java
@@ -3,6 +3,9 @@ package com.portfolio.javabackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -10,6 +13,18 @@ public class JavaBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaBackendApplication.class, args);
+	}
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/app/login").allowedOriginPatterns("http://localhost:8080");
+				WebMvcConfigurer.super.addCorsMappings(registry);
+			}
+		};
 	}
 
 }
